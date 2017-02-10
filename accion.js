f525bd38227f212c0c56e89cbf512587d5ca1782
dcrdn <A01227808@itesm.mx>
2017-02-10 10:46:21 -0600

    Final

---
+++ accion.js
@@ -0,0 +1,15 @@
+function color(){
+   document.getElementById("diego").style.color = "white";
+}
+
+function size(){
+   document.getElementById("cosa").style.fontSize ="34px";
+}
+
+function desaparecer(){
+   document.getElementById("dp").style.display ="none";
+}
+
+function cambiar_color(){
+   document.getElementById("gato").style.background ="white";
+}
